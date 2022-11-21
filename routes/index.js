f2b9c23a69034e42469bfa84c994511a741c8440
Hector Andres Diaz <pipefrtin78@gmail.com>
2022-11-20 21:27:23 -0500

    creacion de consultas Hector Andres

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+//consultas crud Andres
+router.get('/api/personas', personasController.List);
+router.get('/api/personas/id/:id', personasController.ListAt);
+router.delete('/api/personas/id/:id', personasController.eliminarPersonas);
+router.patch('/api/personas/id/:id', personasController.actualizarPersonas);
+router.post('/api/personas/:nombre/:apellido/:telefono/:email/:edad/:residencia/:id_profesion/:id_servicio/:id_registro/:createdAt/:updatedAt', personasController.crearPersona);
+router.get('/api/estados', estadosController.List);
+router.get('/api/estados/id/:id', estadosController.ListAt);
+router.delete('/api/estados/id/:id', estadosController.eliminarEstados);
+router.patch('/api/estados/id/:id', estadosController.actualizarEstados);
+router.post('/api/estados/:pendiente/:aprobado/:rechazado/:createdAt/:updatedAt', estadosController.crearEstado);
 
 module.exports = router;
